a714c292a61f36d2114f1c55147a95a9c61f506b
Kartar Singh Jabanda <kartarsjabanda@gmail.com>
2021-02-01 10:02:56 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import stickyScrollCatch from './index.ts'
+import stickyScrollCatch from '../npm-test/main.ts'
+// import stickyScrollCatch from 'sticky-scroll-catch'
+
+// use npm
+// import css and js here
 
 window.addEventListener('scroll', stickyScrollCatch)
